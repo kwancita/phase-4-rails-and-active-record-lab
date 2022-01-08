39e1a3e6a9634beb0ac22ce3a9c53235547a4018
Wasanta <kwan_u_day@hotmail.com>
2022-01-08 12:37:14 -0500

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create!(first_name: "Kwan", last_name: "Wasanta", grade: 4)
+Student.create!(first_name: "Craig", last_name: "Link", grade: 3)
